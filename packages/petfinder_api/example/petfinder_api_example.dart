4f5f943ce69551e9bee2cad4c692ba14a76efbad
Kehinde Akeredolu <kayzkenny@gmail.com>
2023-04-03 21:48:34 +0100

    create petfinder_api package

---
+++ packages/petfinder_api/example/petfinder_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:petfinder_api/petfinder_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/petfinder_api/lib/petfinder_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library petfinder_api;
+
+export 'src/petfinder_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
