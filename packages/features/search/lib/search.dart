0c75d03c204719231bc0518fa9b42fcbbc626fe2
Kehinde Akeredolu <kayzkenny@gmail.com>
2023-04-25 03:51:56 +0100

    create search package

---
+++ packages/features/search/lib/search.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library search;
+
+export 'src/search_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ packages/features/search/lib/src/search_base.dart
@@ -0,0 +1,11 @@
+// enum AnimalSearchType: String, CaseIterable {
+//   case none
+//   case cat
+//   case dog
+//   case rabbit
+//   case smallAndFurry = "small & furry"
+//   case horse
+//   case bird
+//   case scalesFinsAndOther = "scales, fins & other"
+//   case barnyard
+// }
